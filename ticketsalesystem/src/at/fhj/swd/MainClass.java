117fdf1de4a74045c9fc000e68440b197bcefb3c
bernhardharing <b12101210>
2016-10-05 19:37:02 +0200

    - create folder

--- ticketsalesystem/src/at/fhj/swd/MainClass.java
+++ ticketsalesystem/src/at/fhj/swd/MainClass.java
@@ -4,4 +4,13 @@ package at.fhj.swd;
  * Created by bernhard on 05.10.2016.
  */
 public class MainClass {
+
+    public static void main  (String args[]){
+        System.out.println("hallo");
+        int a = 33;
+
+        String b = "hallo";
+
+
+    }
 }
