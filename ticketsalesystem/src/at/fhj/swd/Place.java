c2d96c2f5820ca752ad8321408e5729802801de8
haring <bernhard.haring@gmx.at>
2016-10-29 12:55:51 +0200

    -- removed one id from place

--- ticketsalesystem/src/at/fhj/swd/Place.java
+++ ticketsalesystem/src/at/fhj/swd/Place.java
@@ -9,14 +9,12 @@ import javax.persistence.*;
 public class Place {
     @Id
     private Integer id;
-    private Integer placeId;
 
     @ManyToOne
     private Category category;
 
     @OneToOne
     private Ticket ticket;
-
 
     @Column(name = "id")
     public Integer getId() {
@@ -27,33 +25,5 @@ public class Place {
         this.id = id;
     }
 
-    @Basic
-    @Column(name = "place_id")
-    public Integer getPlaceId() {
-        return placeId;
-    }
 
-    public void setPlaceId(Integer placeId) {
-        this.placeId = placeId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Place place = (Place) o;
-
-        if (id != null ? !id.equals(place.id) : place.id != null) return false;
-        if (placeId != null ? !placeId.equals(place.placeId) : place.placeId != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (placeId != null ? placeId.hashCode() : 0);
-        return result;
-    }
 }
