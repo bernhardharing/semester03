a42021cd1f2dc26f3cbad8335cb568cdec4a789a
bernhardharing <b12101210>
2016-10-30 10:34:12 +0100

    -- add test customer and categories

---
+++ ticketsalesystem/sql/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (description, price) values ('luxus',100);
+INSERT INTO category (description, price) VALUES ('teuer',50);
+INSERT INTO category (description, price) VALUES ('angebot',20);
+
+Insert Into customer (customer_number, name, address) Values (100, 'Hans Gruber','Hinterberg 1');
+Insert Into customer (customer_number, name, address) Values (101, 'Peter König','Eggenberg 78');
